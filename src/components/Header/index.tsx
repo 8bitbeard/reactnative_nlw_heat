74ddfb7453d2c97aafb7e6162a18ae7eccd5019e
Wilton Souza <willsouzafilho@gmail.com>
2021-10-22 09:31:31 -0300

    Adding signIn and signOut logic

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,21 +5,24 @@ import { UserPhoto } from '../UserPhoto';
 import { styles } from './styles';
 
 import LogoSvg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
 
 export function Header(){
+  const { user, signOut } = useAuth();
   return (
     <View style={styles.container }>
-
-        <LogoSvg />
-
-            <View style={styles.logoutButton}>
-            <TouchableOpacity>
-                <Text style={styles.logoutText}>
-                    Sair
-                </Text>
+      <LogoSvg />
+        <View style={styles.logoutButton}>
+          {
+            user &&
+            <TouchableOpacity onPress={signOut}>
+              <Text style={styles.logoutText}>
+                  Sair
+              </Text>
             </TouchableOpacity>
-            <UserPhoto imageUri='https://github.com/8bitbeard.png' />
+          }
+          <UserPhoto imageUri={user?.avatar_url} />
         </View>
     </View>
   );
-} 
+}
--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,4 +1,12 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import * as AuthSession from 'expo-auth-session';
+import { api } from '../services/api'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const CLIENT_ID = '49f89a774316e6060030';
+const SCOPE = 'read:user';
+const USER_STORAGE = '@reactnative_nlw_heat:user';
+const TOKEN_STORAGE = '@reactnative_nlw_heat:token';
 
 type User = {
     id: string;
@@ -10,28 +18,78 @@ type User = {
 type AuthContextData = {
     user: User | null;
     isSigningIn: boolean;
-    signIn: () => Promise<void>
-    signOut: () => Promise<void>
+    signIn: () => Promise<void>;
+    signOut: () => Promise<void>;
 }
 
 type AuthProviderProps = {
     children: React.ReactNode
-
 }
+
+type AuthResponse = {
+    token: string;
+    user: User;
+}
+
+type AuthorizationResponse = {
+    params: {
+        code?: string
+        error?: string
+    },
+    type?: string
+}
+
 
 export const AuthContext = createContext({} as AuthContextData)
 
 function AuthProvider({ children }: AuthProviderProps) {
-    const [isSigningIn, setIsSigningIn] = useState(false)
+    const [isSigningIn, setIsSigningIn] = useState(true)
     const [user, setUser] = useState<User | null>(null);
 
     async function signIn() {
+        try {
+            setIsSigningIn(true)
+            const authUrl = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=${SCOPE}`
+            const authSessionResponse = await AuthSession.startAsync({ authUrl }) as AuthorizationResponse
 
+            if (authSessionResponse.type === 'success' && authSessionResponse.params.error !== 'access_denied') {
+                const authResponse = await api.post('authenticate', { code: authSessionResponse.params.code });
+                const { user, token } = authResponse.data as AuthResponse;
+
+                api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+                await AsyncStorage.setItem(USER_STORAGE, JSON.stringify(user));
+                await AsyncStorage.setItem(TOKEN_STORAGE, JSON.stringify(token));
+
+                setUser(user);
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsSigningIn(false);
+        }
     }
 
     async function signOut() {
+        setUser(null);
+        await AsyncStorage.removeItem(USER_STORAGE);
+        await AsyncStorage.removeItem(TOKEN_STORAGE);
 
     }
+
+    useEffect(() => {
+        async function loadUserStorageData() {
+            const userStorage = await AsyncStorage.getItem(USER_STORAGE);
+            const tokenStorage = await AsyncStorage.getItem(TOKEN_STORAGE);
+
+            if(userStorage && tokenStorage) {
+                api.defaults.headers.common['Authorization'] = `Bearer ${tokenStorage}`
+                setUser(JSON.parse(userStorage))
+            }
+
+            setIsSigningIn(false);
+        }
+        loadUserStorageData();
+    }, []);
 
     return (
         <AuthContext.Provider value={{
